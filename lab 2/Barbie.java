5db21e1b4950ca845b51153a6b48c0f3bd06b3ef
vinitian <140070514+vinitian@users.noreply.github.com>
2024-01-19 14:30:16 +0700

    Create Barbie.java

---
+++ lab 2/Barbie.java
@@ -0,0 +1,12 @@
+public class Barbie extends Doll {
+
+    public Barbie(String name, double price) {
+        super(name, "Plastic", price);
+
+    }
+
+    public void play() {
+        System.out.println("Barbie sings: I'm a Barbie girl in a Barbie world!");
+    }
+
+}
