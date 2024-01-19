1d3152459c0345e8cf9f7fd2697d126dcb2f6704
vinitian <140070514+vinitian@users.noreply.github.com>
2024-01-19 14:30:45 +0700

    Create PorcelainDoll.java

---
+++ lab 2/PorcelainDoll.java
@@ -0,0 +1,12 @@
+
+public class PorcelainDoll extends Doll {
+
+    public PorcelainDoll(String name, double price) {
+        super(name, "Porcelain", price);
+
+    }
+
+    public void play(){
+        System.out.println("Porcelain Doll is delicate, be gentle!");
+    }
+}
