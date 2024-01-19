c038a388d38d61a806661293ad222d576d18d851
vinitian <140070514+vinitian@users.noreply.github.com>
2024-01-19 14:31:35 +0700

    Create TeddyDoll.java

---
+++ lab 2/TeddyDoll.java
@@ -0,0 +1,12 @@
+public class TeddyDoll extends Doll {
+
+    public TeddyDoll(String name, double price) {
+        super(name, "Fur", price);
+
+    }
+
+    public void play() {
+        System.out.println("Teddy Doll says: Hug me!");
+
+    }
+}
